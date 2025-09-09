de21385f525b6a3b15be64403d7e2b3939ecc48b
FRAZ5094 <frasergmorrison@binternet.com>
2025-09-09 23:43:47 +0100

    added bench marks for sendfile and read write and it doesn't make a difference because write to stdout take so long

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
 default:
 	g++ *.cpp -o main && ./main
+build:
+	g++ *.cpp -o main
+run:
+	./main
+clean:
+	rm main
+	rm *.o
--- main.cpp
+++ main.cpp
@@ -1,11 +1,68 @@
-#include "include/http_server.h"
-#include <cstdint>
+// #include "include/http_server.h"
+// #include <cstdint>
+#include <chrono>
+#include <cstdio>
+#include <fcntl.h>
+// #include <stdio.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void write_sendfile() {
+  int fd = open("index.html", O_RDONLY);
+  struct stat st;
+  fstat(fd, &st);
+
+  //  Write file to stdout
+  sendfile(1, fd, NULL, st.st_size);
+  close(fd);
+}
+void write_read_write() {
+  int fd = open("index.html", O_RDONLY);
+  struct stat st;
+  fstat(fd, &st);
+
+  int file_size = st.st_size;
+
+  char buffer[file_size];
+
+  read(fd, buffer, file_size);
+
+  write(1, buffer, file_size);
+  close(fd);
+}
+
+int N_ITERATIONS = 10000;
+
+float bench_mark(void (*func)()) {
+  long times[N_ITERATIONS];
+
+  for (int i = 0; i < N_ITERATIONS; i++) {
+    auto start = std::chrono::high_resolution_clock::now();
+    func();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    times[i] = duration.count();
+  }
+  long sum = 0;
+
+  for (auto time : times) {
+    sum += time;
+  }
+
+  float avg = (float)sum / N_ITERATIONS;
+
+  return avg;
+}
 
 int main() {
-  uint16_t port;
-  port = 8181;
 
-  HttpServer server(port);
-  server.start();
+  float sendfile_avg = bench_mark(&write_sendfile);
+  float read_write_avg = bench_mark(&write_read_write);
+
+  printf("sendfile avg: %.1f\n", sendfile_avg);
+  printf("read_write avg: %.1f\n", read_write_avg);
+
   return 0;
 }
