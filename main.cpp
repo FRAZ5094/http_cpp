73e2b065eacf41e04185d069b684340dc9a08b97
FRAZ5094 <frasergmorrison@binternet.com>
2025-09-20 00:29:04 +0100

    started work on talking to postgres

--- main.cpp
+++ main.cpp
@@ -1,45 +1,65 @@
-#include "include/http_server.h"
+#include <cinttypes>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/sendfile.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/un.h>
 #include <unistd.h>
 
-#define BYTE_TO_BINARY_PATTERN "0b%c%c%c%c %c%c%c%c"
-#define BYTE_TO_BINARY(byte)                                                   \
-  ((byte) & 0x80 ? '1' : '0'), ((byte) & 0x40 ? '1' : '0'),                    \
-      ((byte) & 0x20 ? '1' : '0'), ((byte) & 0x10 ? '1' : '0'),                \
-      ((byte) & 0x08 ? '1' : '0'), ((byte) & 0x04 ? '1' : '0'),                \
-      ((byte) & 0x02 ? '1' : '0'), ((byte) & 0x01 ? '1' : '0')
+void handle_error(const char *msg) {
+  perror(msg);
+  exit(EXIT_FAILURE);
+}
 
-void get_byte_str(char byte, char *str) {
-  for (int i = 0; i < 8; i++) {
-    if (byte & (1 << i)) {
-      str[i] = '1';
-    } else {
-      str[i] = '0';
-    }
+int send_startup_message(int fd) {
+  int16_t protocol_major_version = 3;
+  int16_t protocol_minor_version = 0;
+  char user_name[] = "postgres";
+  char db[] = "mux_dev";
+  char application_name[] = "raw_c_postgres";
+  char encoding[] = "UTF8";
+
+  char params[] = "user\0fraser\0"
+                  "database\0mux_dev\0"
+                  "application_name\0"
+                  "psql\0"
+                  "client_encoding\0UTF8";
+  // This won't work because the null terminations make strlen think the string
+  // ends early
+  // int32_t msg_len =
+  //     sizeof(int32_t) + sizeof(int16_t) + sizeof(int16_t) + strlen(params);
+  int32_t msg_len = sizeof(int32_t) + sizeof(int16_t) + sizeof(int16_t) + 72;
+  char msg[msg_len];
+  sprintf(msg, "%u%u%u%s", msg_len, protocol_major_version,
+          protocol_minor_version, params);
+  if (send(fd, msg, sizeof(msg), 0) == -1) {
+    handle_error("send");
+    return -1;
   }
-  str[8] = '\0';
-};
+  return 0;
+}
 
 int main() {
+  int sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 
-  HttpServer server(8181);
+  struct sockaddr_un addr{.sun_family = AF_UNIX,
+                          .sun_path = "/var/run/postgresql/.s.PGSQL.5432"};
 
-  char mask = 0xFF;
-
-  mask &= ~(1 << 3);
-
-  // char str[9];
-  // get_byte_str(mask, str);
-
-  // printf("0b%s\n", str);
-  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(mask));
-
-  while (1) {
+  if (connect(sock_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    handle_error("connect");
   }
-}
+
+  printf("Connected to postgres\n");
+
+  int ret = send_startup_message(sock_fd);
+  sleep(1);
+  char buff[1024];
+  int n = read(sock_fd, buff, sizeof(buff));
+
+  close(sock_fd);
+};
